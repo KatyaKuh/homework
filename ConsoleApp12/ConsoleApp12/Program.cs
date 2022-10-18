0a4c503bc9670493fe7b4ecfb29943e33a510f7d
KatyaKuh <116082214+KatyaKuh@users.noreply.github.com>
2022-10-18 15:17:52 +0300

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 345;
+
+            int sot = c / 100;
+            int des = c / 10 % 10;
+            int ed = c % 10;
+
+            Console.WriteLine($"{sot},{des},{ed}");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
